ed5c712be08e7c13a9bd3281f5ad6ed19c1a31d6
jotasprout <jotasprout@gmail.com>
2017-10-21 14:55:35 -0400

    building higher

--- app2.php
+++ app2.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+require 'artists.php';
 require 'vendor/autoload.php';
 
 // Fetch saved access token
@@ -9,13 +10,12 @@ $accessToken = $_SESSION['accessToken'];
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 $api->setAccessToken($accessToken);
 
+$artistID = $davidBowie;
+
 // It's now possible to request data from the Spotify catalog
 print_r(
-    $api->getArtist('3EhbVgyfGd7HkpsagwL9GS')
+    $api->getArtist($artistID)
 );
-
-// as always, starting off dev with Alice Cooper for testing
-$artistID = '3EhbVgyfGd7HkpsagwL9GS';
 
 $artistName = '';
 $artistPop = '';
@@ -39,3 +39,57 @@ $albumTracksURL = '';
 $albumTracksIDs = array ();
 
 ?>
+
+<html>
+
+<head>
+  <meta charset="UTF-8">
+    <title>Pop-PHP</title>
+    <script src='https://www.roxorsoxor.com/js/jquery-214.js'></script>
+</head>
+
+<body>
+	<div class="container">
+
+        <form class="form-horizontal" id="rockinForm">
+            <fieldset>
+                <legend>PopRock</legend>
+
+                <div class="form-group"> <!-- Row 1 -->
+                    <label class="col-lg-2 control-label" for="artist">Ye Olde Select An Artist Menu</label>
+                    <div class="col-lg-4">
+                        <select class="form-control" id="artist" name="artist">
+                            <option value="">- Choose -</option>
+                            <option value="0oSGxfWSnnOXhD2fKuz2Gy">David Bowie</option>
+                            <option value="74ASZWbe4lXaubB36ztrGX">Bob Dylan</option>
+                            <option value="7dnB1wSxbYa8CejeVg98hz">Meat Loaf</option>
+                            <option value="3EhbVgyfGd7HkpsagwL9GS">Alice Cooper</option>
+                            <option value="3lPQ2Fk5JOwGWAF3ORFCqH">John Mellencamp</option>
+                        </select>
+                    </div>
+                </div><!-- /Row 1 -->
+
+                <div class="form-group"> <!-- Row 2 -->
+                    <div class="col-lg-4 col-lg-offset-2">
+                        <button class="btn btn-primary" type="button" id="getArtistButt">Get Artist Object</button>
+                    </div>
+                </div><!-- /Row 2 -->
+            </fieldset>
+        </form>
+
+        <h2></h2>
+        <table id="albums" class="table table-striped table-hover ">
+            <tr>
+              <th>Album ID</th>
+              <th>Album Name</th>
+              <th>Album Released</th>
+              <th>Album Popularity</th>
+            </tr>
+        </table>
+
+	</div> <!-- /container -->
+
+    <footer class="footer"><p>&copy; Sprout Means Grow 2017</p></footer>
+
+<script src="createArtistObject5.js"></script>
+</body>
---
+++ artists.php
@@ -0,0 +1,16 @@
+<?php
+
+$aliceCooper = "3EhbVgyfGd7HkpsagwL9GS";
+$davidBowie = "0oSGxfWSnnOXhD2fKuz2Gy";
+$bobDylan = "74ASZWbe4lXaubB36ztrGX";
+$meatLoaf = "7dnB1wSxbYa8CejeVg98hz";
+$johnMellencamp = "3lPQ2Fk5JOwGWAF3ORFCqH";
+$stooges = "4BFMTELQyWJU1SwqcXMBm3";
+$bruceSpringsteen = "3eqjTLE0HfPfh78zjh6TqT";
+$kiss = "07XSN3sPlIlB2L2XNcTwJw";
+$iggyPop = "33EUXrFKGjpUSGacqEHhU4";
+$popWilliamson = "1l8grPt6eiOS4YlzjIs0LF";
+$tomPetty = "2UZMlIwnkgAEDBsw1Rejkn";
+$tpHeartbreakers = "4tX2TplrkIP4v05BNC903e";
+
+?>
