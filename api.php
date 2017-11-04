d3649843f320c02339a0e3e3ef51f123df3e929c
Jota Sprout <jotasprout@gmail.com>
2017-11-04 10:21:09 -0400

    Deleted v5 and other Disaster

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Fetch saved access token
-$accessToken = $_SESSION['accessToken'];
-
-$GLOBALS['api'] = new SpotifyWebAPI\SpotifyWebAPI();
-$GLOBALS['api']->setAccessToken($accessToken);
-
-?>
