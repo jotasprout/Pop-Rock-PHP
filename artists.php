a8c7ee350c55d47841af2e1d19b9a68b458612ff
jotasprout <jotasprout@gmail.com>
2017-11-09 11:03:06 -0500

    building new insert artists scripts

--- artists.php
+++ artists.php
@@ -21,8 +21,29 @@ $travelingWilburys = new artist("2hO4YtXUFJiUYS2uYFvHNK","Traveling Wilburys");
 
 $artists = array ("3k4YA0uPsWc2PuOQlJNpdH","3EhbVgyfGd7HkpsagwL9GS","0oSGxfWSnnOXhD2fKuz2Gy","74ASZWbe4lXaubB36ztrGX","7dnB1wSxbYa8CejeVg98hz","3lPQ2Fk5JOwGWAF3ORFCqH","4BFMTELQyWJU1SwqcXMBm3","3eqjTLE0HfPfh78zjh6TqT","07XSN3sPlIlB2L2XNcTwJw","33EUXrFKGjpUSGacqEHhU4","1l8grPt6eiOS4YlzjIs0LF","22bE4uQ6baNwSHPVcDxLCe","2UZMlIwnkgAEDBsw1Rejkn","4tX2TplrkIP4v05BNC903e");
 
-$artists2 = array ("2hO4YtXUFJiUYS2uYFvHNK");
+$BonJovi = ("Bon Jovi", "58lV9VcRSjABbAbfWS6skp");
+$KateBush = ("Kate Bush", "1aSxMhuvixZ8h9dK9jIDwL");
+$Cars = ("The Cars", "6DCIj8jNaNpBz8e5oKFPtp");
+$DepecheMode = ("Depeche Mode", "762310PdDnwsDxAQxzQkfX");
+$DireStraits = ("Dire Straits", "0WwSkZ7LtFUFjGjMZBMt6T");
+$Eurythmics = ("Eurythmics", "0NKDgy9j66h3DLnN8qu1bB");
+$JGeilsBand = ("J. Geils Band", "69Mj3u4FTUrpyeGNSIaU6F");
+$JudasPriest = ("Judas Priest", "2tRsMl4eGxwoNabM08Dm4I");
+$LLCoolJ = ("LL Cool J", "1P8IfcNKwrkQP5xJWuhaOC");
+$MC5 = ("The MC5", "4WquJweZPIK9qcfVFhTKvf");
+$TheMeters = ("The Meters", "2JRvXPGWiINrnJljNJhG5s");
+$MoodyBlues = ("Moody Blues", "5BcZ22XONcRoLhTbZRuME1");
+$Radiohead = ("Radiohead", "4Z8W4fKeB5YxbusRsdQVPb");
+$RageAgainstTheMachine = ("Rage Against the Machine", "2d0hyoQ5ynDBnkvAbJKORj");
+$RufusFeatChakaKhan = ("Rufus featuring Chaka Khan", "1YLsqPcFg1rj7VvhfwnDWm");
+$NinaSimone = ("Nina Simone", "7G1GBhoKtEPnP86X2PvEYO");
+$SisterRosettaTharpe = ("Sister Rosetta Tharpe", "2dXf5lu5iilcaTQJZodce7");
+$LinkWray = ("Link Wray", "2vQavlZtDA660mnZotYIto");
+$Zombies = ("The Zombies", "2jgPkn6LuUazBoBk6vvjh5");
+$TravelingWilburys = ("Traveling Wilburys", "2hO4YtXUFJiUYS2uYFvHNK");
+$ChakaKhan = ("Chaka Khan", "6mQfAAqZGBzIfrmlZCeaYT");
 
+$2018nominees = array ();
 
 function getArtistsPop ($artists) {
 				
