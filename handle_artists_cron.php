7239142b341e91871a6397da34722a0d2972d7b5
Jota Sprout <jotasprout@gmail.com>
2017-11-18 16:30:43 -0500

    Final Artist Cron

--- handle_artists_cron.php
+++ handle_artists_cron.php
@@ -1,11 +1,19 @@
 <?php
 
 session_start();
-
+require 'spotifySecrets.php';
 require 'vendor/autoload.php';
 require_once 'rockdb.php';
 require_once 'artists.php';
 
+$session = new SpotifyWebAPI\Session($myClientID, $myClientSecret);
+
+$session->requestCredentialsToken();
+$accessToken = $session->getAccessToken();
+
+// I don't think the cron needs this next line 
+$_SESSION['accessToken'] = $accessToken;
+// and I don't think the cron needs this next line either
 $accessToken = $_SESSION['accessToken'];
 
 $GLOBALS['api'] = new SpotifyWebAPI\SpotifyWebAPI();
@@ -13,5 +21,7 @@ $GLOBALS['api']->setAccessToken($accessToken);
 
 getArtistsPopCron ($artists);
 getArtistsPopCron ($nominees2018);
+
+die();
 
 ?>
