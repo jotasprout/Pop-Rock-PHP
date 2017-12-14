f7df82a3bf48e6c232261d68fceca1c0f1ca9314
Jota Sprout <jotasprout@gmail.com>
2017-12-13 19:19:54 -0500

    Hopeful tutes and examples for json conversion

--- getDataAndConvertToD3.php
+++ getDataAndConvertToD3.php
@@ -18,7 +18,7 @@ function artistsD3 () {
 								FROM popArtists b2)
 		ORDER BY b.pop ASC";
 
-	$jsonData = array ();
+	
 
 	$getit = $connekt->query($artistInfoRecent);
 
@@ -30,5 +30,86 @@ function artistsD3 () {
 }
 
 artistsD3();
+
+// SOMETHING I FOUND 
+function console_log( $data ){
+	echo '<script>';
+	echo 'console.log('. json_encode( $data ) .')';
+	echo '</script>';
+  }
+
+// A MYSQLI EXAMPLE
+
+  // Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql    = "SELECT * FROM cpu";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $rows = array();
+    while ($row = mysqli_fetch_array($result)) {
+
+        $rows[] = $row;
+    }
+
+    echo json_encode($rows);
+} else {
+    echo "no results found";
+}
+
+mysqli_close($conn);
+
+// A PDO EXAMPLE
+
+$host    = 'localhost';
+$db      = 'sanpham';
+$user    = 'root';
+$pass    = '';
+$charset = 'utf8';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+
+$dbh = new PDO($dsn, $user, $pass, $opt);
+
+$sql  = $dbh->query("SELECT * FROM cpu");
+$rows = array();
+while ($row = $sql->fetchall()) {
+    $rows[] = $row;
+}
+echo json_encode($rows);
+
+
+// FROM PSYCHO CODES
+
+$host = "localhost";
+$db = "psychocodes";
+$user = "your username";  //enter your database username
+$pass = "your password";  //enter your database password
+$conn = new mysqli($host,$user,$pass,$db); 
+$rows = array();
+
+$sql = "SELECT * FROM data";
+$result = $conn->query($sql) or die("cannot write");
+while($row = $result->fetch_assoc()){
+	$rows[] = $row;
+}
+
+echo "<pre>";
+print json_encode(array('serverres'=>$rows));
+echo "</pre>";
+
+
+?>
 
 ?>
