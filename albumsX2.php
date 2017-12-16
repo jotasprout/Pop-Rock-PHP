dc3fa4d91ec8cb7db866325751949065ff9ead1e
jotasprout <jotasprout@gmail.com>
2017-12-16 16:03:20 -0500

    maybe images

--- albumsX2.php
+++ albumsX2.php
@@ -282,16 +282,37 @@ function showAlbums ($artistID) {
 					ON a.albumID = p1.albumID
 					ORDER BY a.year ASC;";
 
+$happyScabies2 = "	SELECT a.albumName, a.year, a.albumArt, z.artistName, p1.pop, p1.date
+					FROM (SELECT
+								y.albumID AS albumID,
+								y.albumName AS albumName,
+								y.artistID AS artistID,
+								y.albumArt AS albumArt,
+								y.year AS year
+							FROM albums y 
+							WHERE y.artistID = '$artistID') a
+					JOIN artists z ON z.artistID = '$artistID'
+					JOIN (SELECT p.*
+							FROM popAlbums p
+							INNER JOIN (SELECT albumID, pop, max(date) AS MaxDate
+										FROM popAlbums  
+										GROUP BY albumID) groupedp
+							ON p.albumID = groupedp.albumID
+							AND p.date = groupedp.MaxDate) p1 
+					ON a.albumID = p1.albumID
+					ORDER BY a.year ASC;";					
+
 	// the next line works in stakeout but not here
 	// $result = $connekt->query($query);
 
-	$result = mysqli_query($connekt,$happyScabies);
+	$result = mysqli_query($connekt,$happyScabies2);
 
 	$rows = array();
 
 	while ($row = mysqli_fetch_array($result)) {
 		// $artistID = $row["artistID"];
 		$artistName = $row['artistName'];
+		$albumArt = $row['albumArt'];
 		$albumName = $row['albumName'];
 		$albumReleased = $row['year'];
 		$albumPop = $row['pop'];
@@ -299,7 +320,7 @@ function showAlbums ($artistID) {
 		$rows[] = $row;
 		
 		echo "<tr>";
-		echo "<td>" . $artistName . "</td>";
+		echo "<td><img src='" . $albumArt . "' height='64' width='64'></td>";
 		echo "<td>" . $albumName . "</td>";
 		echo "<td>" . $albumReleased . "</td>";
 		echo "<td>" . $albumPop . "</td>";
@@ -307,7 +328,8 @@ function showAlbums ($artistID) {
 
 	}
 
-	echo json_encode($rows);
+	// echo json_encode($rows);
+	// make the above echo a js script that sends the json to the console
 
 }
 
