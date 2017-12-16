2b1cbc544534e7f5518e108d35b1d9fca91cead4
Jota Sprout <jotasprout@gmail.com>
2017-12-15 23:29:10 -0500

    Xian artist updated menu

--- choose_xianArtist.php
+++ choose_xianArtist.php
@@ -58,6 +58,31 @@ echo "<option value='" . $resurrectionBand->get_artistID() . "'>" . $resurrectio
 echo "<option value='" . $xlDBD->get_artistID() . "'>" . $xlDBD->get_artistName() . "</option>";
 echo "<option value='" . $crucified->get_artistID() . "'>" . $crucified->get_artistName() . "</option>";
 echo "<option value='" . $playdough->get_artistID() . "'>" . $playdough->get_artistName() . "</option>";
+
+echo "<option value='" . $saint->get_artistID() . "'>" . $saint->get_artistName() . "</option>";
+echo "<option value='" . $twelthTribe->get_artistID() . "'>" . $twelthTribe->get_artistName() . "</option>";
+echo "<option value='" . $seventySevens->get_artistID() . "'>" . $seventySevens->get_artistName() . "</option>";
+echo "<option value='" . $mortification->get_artistID() . "'>" . $mortification->get_artistName() . "</option>";
+echo "<option value='" . $klank->get_artistID() . "'>" . $klank->get_artistName() . "</option>";
+echo "<option value='" . $livingSacrifice->get_artistID() . "'>" . $livingSacrifice->get_artistName() . "</option>";
+echo "<option value='" . $sayWhat->get_artistID() . "'>" . $sayWhat->get_artistName() . "</option>";
+echo "<option value='" . $barrenCross->get_artistID() . "'>" . $barrenCross->get_artistName() . "</option>";
+echo "<option value='" . $jerusalem->get_artistID() . "'>" . $jerusalem->get_artistName() . "</option>";
+echo "<option value='" . $lustControl->get_artistID() . "'>" . $lustControl->get_artistName() . "</option>";
+echo "<option value='" . $argylePark->get_artistID() . "'>" . $argylePark->get_artistName() . "</option>";
+echo "<option value='" . $deliverance->get_artistID() . "'>" . $deliverance->get_artistName() . "</option>";
+echo "<option value='" . $tourniquet->get_artistID() . "'>" . $tourniquet->get_artistName() . "</option>";
+echo "<option value='" . $mortal->get_artistID() . "'>" . $mortal->get_artistName() . "</option>";
+echo "<option value='" . $circleDust->get_artistID() . "'>" . $circleDust->get_artistName() . "</option>";
+echo "<option value='" . $deitiphobia->get_artistID() . "'>" . $deitiphobia->get_artistName() . "</option>";
+echo "<option value='" . $rosarium->get_artistID() . "'>" . $rosarium->get_artistName() . "</option>";
+echo "<option value='" . $mxpx->get_artistID() . "'>" . $mxpx->get_artistName() . "</option>";
+echo "<option value='" . $whitecross->get_artistID() . "'>" . $whitecross->get_artistName() . "</option>";
+echo "<option value='" . $ninetyPoundWuss->get_artistID() . "'>" . $ninetyPoundWuss->get_artistName() . "</option>";
+echo "<option value='" . $ghotiHook->get_artistID() . "'>" . $ghotiHook->get_artistName() . "</option>";
+echo "<option value='" . $fiveIronFrenzy->get_artistID() . "'>" . $fiveIronFrenzy->get_artistName() . "</option>";
+echo "<option value='" . $larryNorman->get_artistID() . "'>" . $larryNorman->get_artistName() . "</option>";
+echo "<option value='" . $scaterdFew->get_artistID() . "'>" . $scaterdFew->get_artistName() . "</option>";
 ?>
 
                     </select>
