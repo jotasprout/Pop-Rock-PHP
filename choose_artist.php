b1a39253e411c6e26fbe7056cd98a962f48212cb
jotasprout <jotasprout@gmail.com>
2018-03-02 09:17:15 -0500

    Session Artist is null on chooseArtist open

--- choose_artist.php
+++ choose_artist.php
@@ -3,6 +3,8 @@
     require_once 'auth.php';
     // Fetch saved access token
     // $accessToken = $_SESSION['accessToken'];
+    // Will next line fix the lurking legacy layover of previous artist's tracks?
+    $_SESSION['artist'] = "";
 
     // site pages only get from my DB so they don't need auth
     require_once 'rockdb.php';
