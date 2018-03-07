f2a173c5bddd3a9e0fd6cbdc115286d7e999790c
JotaSprout <jotasprout@gmail.com>
2018-03-06 19:28:58 -0500

    Little Thing

--- navbar_rock.php
+++ navbar_rock.php
@@ -1,19 +1,19 @@
 <?php
 
 $navbar = "
-
-<nav class='navbar navbar-default'>	
-	<div id='header' class='container-fluid'>		
-		<ul class='nav navbar-nav'>		
-			<li><a id='home' href='https://www.roxorsoxor.com/poprock/choose_artist.php'>Choose Artist</a></li>	
-			<li><a id='home' href='https://www.roxorsoxor.com/poprock/this_artist.php'>This Artist</a></li>	
-			<li><a href='https://roxorsoxor.com/poprock/handle_albums.php'>spotAlbums</a></li>	
-			<li><a href='https://roxorsoxor.com/poprock/show_myAlbums.php'>myAlbums</a></li>				
-			<li><a href='https://roxorsoxor.com/poprock/handle_tracks.php'>spotTracks</a></li>
-			<li><a href='https://roxorsoxor.com/poprock/show_myTracks.php'>myTracks</a></li>
-			<li><a id='cases' href='https://roxorsoxor.com/poprock/show_myArtists.php'>myArtists</a></li>		
-		</ul>
-	</div> <!-- /container-fluid -->   
-</nav> <!-- /navbar -->";
+	<nav class='navbar navbar-default'>	
+		<div id='header' class='container-fluid'>		
+			<ul class='nav navbar-nav'>		
+				<li><a id='home' href='https://www.roxorsoxor.com/poprock/choose_artist.php'>Choose Artist</a></li>	
+				<li><a id='home' href='https://www.roxorsoxor.com/poprock/this_artist.php'>This Artist</a></li>	
+				<li><a href='https://roxorsoxor.com/poprock/handle_albums.php'>spotAlbums</a></li>	
+				<li><a href='https://roxorsoxor.com/poprock/show_myAlbums.php'>myAlbums</a></li>				
+				<li><a href='https://roxorsoxor.com/poprock/handle_tracks.php'>spotTracks</a></li>
+				<li><a href='https://roxorsoxor.com/poprock/show_myTracks.php'>myTracks</a></li>
+				<li><a id='cases' href='https://roxorsoxor.com/poprock/show_myArtists.php'>myArtists</a></li>		
+			</ul>
+		</div> <!-- /container-fluid -->   
+	</nav> <!-- /navbar -->
+";
 
 ?>
