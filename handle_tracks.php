840ec57c538f26157bc0dd805326c2374ad12528
jotasprout <jotasprout@gmail.com>
2018-03-07 20:11:55 -0500

    maybe who knows

--- handle_tracks.php
+++ handle_tracks.php
@@ -1,8 +1,8 @@
 <?php
 
 include 'sesh.php';
-// $artistID = $_SESSION['artist'];
-// $_SESSION['artist'] = $artistID;
+$artistID = $_SESSION['artist'];
+$_SESSION['artist'] = $artistID;
 
 require_once 'auth.php';
 require_once 'navbar_rock.php';
