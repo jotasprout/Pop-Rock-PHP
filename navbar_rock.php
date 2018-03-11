f068a2f2fea13e9692bfe269c7d18768488cb92f
JotaSprout <jotasprout@gmail.com>
2018-03-11 18:29:47 -0400

    Spring Cleaning Again

--- navbar_rock.php
+++ navbar_rock.php
@@ -6,9 +6,7 @@ $navbar = "
 			<ul class='nav navbar-nav'>		
 				<li><a id='home' href='https://www.roxorsoxor.com/poprock/choose_artist.php'>Choose Artist</a></li>	
 				<li><a id='home' href='https://www.roxorsoxor.com/poprock/this_artist.php'>This Artist</a></li>	
-				<li><a href='https://roxorsoxor.com/poprock/handle_albums.php'>spotAlbums</a></li>	
 				<li><a href='https://roxorsoxor.com/poprock/show_myAlbums.php'>myAlbums</a></li>				
-				<li><a href='https://roxorsoxor.com/poprock/handle_tracks.php'>spotTracks</a></li>
 				<li><a href='https://roxorsoxor.com/poprock/show_myTracks.php'>myTracks</a></li>
 				<li><a id='cases' href='https://roxorsoxor.com/poprock/show_myArtists.php'>myArtists</a></li>		
 			</ul>
