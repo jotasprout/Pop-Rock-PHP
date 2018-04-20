f5e88e87e994359aaeda19a0484789a6868d8b6e
jotasprout <jotasprout@gmail.com>
2018-04-20 16:07:24 -0400

    fixed show all artists

---
+++ show_sortableArtists3.php
@@ -0,0 +1,119 @@
+<?php
+
+include 'sesh.php';
+require_once 'rockdb.php';
+require_once 'navbar_rock.php';
+require_once 'stylesAndScripts.php';
+// require_once 'artists.php';
+
+$connekt = new mysqli( $GLOBALS[ 'host' ], $GLOBALS[ 'un' ], $GLOBALS[ 'magicword' ], $GLOBALS[ 'db' ] );
+
+if ( !$connekt ) {
+	echo 'Darn. Did not connect.';
+};
+
+$artistInfoRecentNoArtYet = "SELECT a.artistID AS artistID, a.artistName AS artistName, p1.pop AS pop, p1.date AS date
+    FROM artists a
+    JOIN (SELECT p.*
+			FROM popArtists p
+			INNER JOIN (SELECT artistID, pop, max(date) AS MaxDate
+						FROM popArtists  
+						GROUP BY artistID) groupedp
+			ON p.artistID = groupedp.artistID
+			AND p.date = groupedp.MaxDate) p1
+	ON a.artistID = p1.artistID
+	ORDER BY p1.pop DESC";
+
+$getit = $connekt->query( $artistInfoRecentNoArtYet );
+
+if(!$getit){ echo 'Cursed-Crap. Did not run the query.'; }	
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Artists Popularity</title>
+	<?php echo $stylesAndSuch; ?>
+	<script src='node_modules/idb/lib/idb.js'></script>
+</head>
+
+<body>
+
+	<div class="container">
+		<?php echo $navbar ?>
+
+		<!-- main -->
+
+		<div class="panel panel-primary">
+
+			<div class="panel-heading">
+				<h3 class="panel-title">Most Recent Artist Popularity in My DB</h3>
+			</div>
+
+			<div class="panel-body">
+
+				<!-- Panel Content -->
+				<!-- D3 chart goes here -->
+				<?php if (!empty($getit)) { ?>
+
+				<table class="table" id="tableoartists">
+					<thead>
+						<tr>
+							<th onClick="sortColumn('artistName', 'ASC')"><div class="pointyHead">Artist Name</div></th>
+							<th onClick="sortColumn('pop', 'DESC')"><div class="pointyHead">Popularity</div></th>
+							<th>Date</th>
+							<!--
+							--> 
+						</tr>
+					</thead>
+
+					<tbody>
+
+					<?php
+					while ( $row = mysqli_fetch_array( $getit ) ) {
+						$artistName = $row[ "artistName" ];
+						$artistPop = $row[ "pop" ];
+						$popDate = $row[ "date" ];
+						?>
+
+					<tr>
+						<td><?php echo $artistName ?></td>
+						<td><?php echo $artistPop ?></td>
+						<td><?php echo $popDate ?></td>
+						<!--
+						-->
+					</tr>
+
+					<?php 
+						} // end of while
+					?>
+
+					</tbody>
+				</table>
+				<?php 
+					} // end of if
+				?>
+
+			</div>
+			<!-- panel body -->
+
+		</div>
+		<!-- panel panel-primary -->
+
+	</div>
+	<!-- close container -->
+
+	<?php echo $scriptsAndSuch; ?>
+	<script src="https://www.roxorsoxor.com/poprock/sortTheseArtists2.js"></script>
+
+	<script>
+	
+	</script>
+
+</body>
+
+</html>
