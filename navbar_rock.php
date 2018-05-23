b9661c5b961f448160671371ca23dcaed44e89a0
jotasprout <jotasprout@gmail.com>
2018-05-23 16:31:12 -0400

    added D3 artist chart to nav

--- navbar_rock.php
+++ navbar_rock.php
@@ -6,6 +6,7 @@ $navbar = "
 			<ul class='nav navbar-nav'>		
 				<li><a id='home' href='https://www.roxorsoxor.com/poprock/choose_artist.php'>Choose Artist</a></li>	
 				<li><a id='home' href='https://www.roxorsoxor.com/poprock/this_sortableArtist.php'>This Artist</a></li>	
+				<li><a id='home' href='https://www.roxorsoxor.com/poprock/forD3/this_D3Artist.php'>This Artist Chart</a></li>
 				<li><a href='https://roxorsoxor.com/poprock/show_sortableAlbums2.php'>Album List</a></li>
 				<li><a href='https://roxorsoxor.com/poprock/forD3/createD3withSVGandArt2.php'>Album Chart</a></li>				
 				<li><a href='https://roxorsoxor.com/poprock/show_sortableTracks.php'>All Tracks</a></li>
