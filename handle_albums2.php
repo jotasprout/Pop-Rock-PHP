0b39bccbdbf584c7d89be54a618fa27e3710ed6f
JotaSprout <jotasprout@gmail.com>
2018-07-29 18:30:34 -0400

    New Albums and Tracks

---
+++ handle_albums2.php
@@ -0,0 +1,84 @@
+<?php
+include 'sesh.php';
+require_once 'auth.php';
+
+require_once 'rockdb.php';
+
+$artistID = "4JeqRr8Upw5uxLEu6jgIRm";
+
+require_once 'navbar_rock.php';
+require_once 'stylesAndScripts.php';
+require_once 'albums.php';
+require_once 'artists.php';
+require_once 'artists_arrays_objects.php';
+
+// Fetch saved access token
+$accessToken = $_SESSION['accessToken'];
+
+$GLOBALS['api'] = new SpotifyWebAPI\SpotifyWebAPI();
+$GLOBALS['api']->setAccessToken($accessToken);
+
+// could these be methods in the artist class?    
+$artist = $GLOBALS['api']->getArtist($artistID);
+$artistName = $artist->name;
+$artistPop = $artist->popularity;
+
+?>
+
+<!DOCTYPE html><html>
+<head><meta charset="UTF-8"><title>Latest Album Data from Spotify</title><?php echo $stylesAndSuch; ?></head>
+<body>
+
+<div class="container">
+
+	<?php echo $navbar ?>
+	
+	<!-- main -->
+	
+<div class="panel panel-primary">
+	<div class="panel-heading">
+		<h3 class="panel-title">Albums</h3>
+	</div>
+	<div class="panel-body"> 
+		
+		<!-- Panel Content --> 
+
+<?php
+echo '<table class="table">';
+echo '<tr><th>Album Cover</th><th>Album Name</th><th>Released</th><th>Popularity</th></tr>';
+
+$discography = $GLOBALS['api']->getArtistAlbums($artistID, [
+	'market' => 'us',
+	'album_type' => 'album',
+	'limit' => '50'
+]);
+
+// should be method in albums class
+foreach ($discography->items as $album) {
+	
+	// Get each albumID for requesting Full Album Object with popularity
+	$albumID = $album->id;
+	
+	// Put albumIDs in array for requesting several at a time (far fewer requests)
+	$artistAlbums [] = $albumID;
+	
+}
+
+// $howmanytotal = count($artistAlbums);
+// echo $howmanytotal . '<br>';
+divideCombineAlbums ($discography);
+// getAlbumsPop ($artistAlbums);
+// divideCombineAlbumsForArt ($artistAlbums);
+
+?>
+
+</table>
+</div> <!-- panel body -->
+<footer class="footer"><p>&copy; Sprout Means Grow and RoxorSoxor 2018</p></footer>
+</div> <!-- panel panel-primary -->
+    </div> <!-- closing container -->
+
+<?php echo $scriptsAndSuch; ?>
+
+</body>
+</html>
