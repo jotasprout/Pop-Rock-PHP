9139cde67bb071d50004c37d9eb7a91e763e8ffc
JotaSprout <jotasprout@gmail.com>
2018-07-30 21:34:44 -0400

    Working Toward Groups

---
+++ artists_groups.php
@@ -0,0 +1,36 @@
+<?php
+
+# Generation X and Billy Idol
+$group_idol = array ("1inWec2E2UgfzMAhwjgTXe", "7lzordPuZEXxwt9aoVZYmG");
+
+# Hollywood Vampires and Alice Cooper 
+$group_alice = array ("3k4YA0uPsWc2PuOQlJNpdH", "3EhbVgyfGd7HkpsagwL9GS");
+
+#Chagall Guevara, Steve Taylor, Perfect Foil, Danielson Foil
+$group_steveTaylor = array ("0p9uD4WGPHqMicwXm3Kavk", "0t1uzfQspxLvAifZLdmFe2", "5MQsxr7sbsewUTIEEYxauR", "0PGxNwykt4KgnvSnNHVUSZ");
+
+# Nugent and Amboy Dukes waiting for Damn Yankees?
+$group_nuge = array ("21ysNsPzHdqYN2fQ75ZswG", "5cVLuEqb7aOHuzwssXHzWI");
+
+# Ozzy, Heaven & Hell, Dio, Black Sabbath
+$group_sabbath = array ("6ZLTlhejhndI4Rh53vYhrY", "4UjiBRkTw9VmvDZiJZKPJ7", "4CYeVo5iZbtYGBN4Isc3n6", "5M52tdBnJaKSvOpJGz8mfZ");
+
+# Ozzy, Black Sabbath
+$group_ozzy = array ("6ZLTlhejhndI4Rh53vYhrY", "5M52tdBnJaKSvOpJGz8mfZ");
+
+# Heaven & Hell, Dio, Black Sabbath, Rainbow
+$group_dio = array ("4UjiBRkTw9VmvDZiJZKPJ7", "4CYeVo5iZbtYGBN4Isc3n6", "5M52tdBnJaKSvOpJGz8mfZ", "6SLAMfhOi7UJI0fMztaK0m");
+
+# Tom Petty, Heartbreakers, Traveling Wilburys
+$group_petty = array ("2UZMlIwnkgAEDBsw1Rejkn", "4tX2TplrkIP4v05BNC903e", "2hO4YtXUFJiUYS2uYFvHNK");
+
+# Iggy, Pop & Williamson, Stooges
+$group_iggy = array ("33EUXrFKGjpUSGacqEHhU4", "1l8grPt6eiOS4YlzjIs0LF", "4BFMTELQyWJU1SwqcXMBm3");
+
+# Rod Stewart waiting for FACES
+$group_rod = array ("2y8Jo9CKhJvtfeKOsYzRdT");
+
+# Comedy
+$comedy = array ("4JeqRr8Upw5uxLEu6jgIRm", "0zokIKbH4c7crvZP9Xel1Y", "1Bd4UVlqlaKEXYRG3wgrCK", "0TorzrcTerTn7l5q9LVjmt", "1Zq8pfBl4ejCMrWdeAdphc");
+
+?>
