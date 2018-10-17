5f60249185cefab05cd673b94f9236150f4b58b8
JotaSprout <jotasprout@gmail.com>
2018-10-17 18:55:47 -0400

    individual artist working

--- functions/add_new_artist.php
+++ functions/add_new_artist.php
@@ -1,7 +1,8 @@
 <?php
 
 session_start();
-require '../secrets/spotifySecrets.php';
+// require '../secrets/spotifySecrets.php';
+require '../secrets/auth.php';
 require '../vendor/autoload.php';
 require_once '../rockdb.php';
 require_once '../functions/artists.php';
@@ -22,31 +23,43 @@ $GLOBALS['api']->setAccessToken($accessToken);
 
 $baseURL = "https://api.spotify.com/v1/artists/";
 
-$artistID = $_POST['artist'];
+// $artistID = $_POST['artist'];
+$thisArtist = '20ge6LnG4KYzVj0Ecj7yDv';
+echo $thisArtist;
 
-echo $artistID;
 
-$getThisArtist = $GLOBALS['api']->getArtist($artistID);
 
-function addArtist ($getThisArtist) {
+function addArtist ($thisArtist) {
+
+    echo $thisArtist;
+
+    $artist = $GLOBALS['api']->getArtist($thisArtist);
 			
     $connekt = new mysqli($GLOBALS['host'], $GLOBALS['un'], $GLOBALS['magicword'], $GLOBALS['db']);
+
     if(!$connekt){
         echo 'Fiddlesticks! Could not connect to database.<br>';
     }
+
     $artistID = $artist->id;
+    echo $artistID;
     $artistNameYucky = $artist->name;
     $artistName = mysqli_real_escape_string($connekt,$artistNameYucky);
+    echo $artistName;
     $artistArt = $artist->images[0]->url;
+    echo $artistArt;
     $artistPop = $artist->popularity;
+    echo $artistPop;
     $insertArtistsInfo = "INSERT INTO artists (artistID,artistName, artistArt) VALUES('$artistID','$artistName', '$artistArt')";
     $rockout = $connekt->query($insertArtistsInfo);
+
     if(!$rockout){
-    echo 'Cursed-Crap. Could not insert artist ' . $artistName . '.<br>';
+        echo 'Cursed-Crap. Could not insert artist ' . $artistName . '.<br>';
     }
 
     $insertArtistsPop = "INSERT INTO popArtists (artistID,pop) VALUES('$artistID','$artistPop')";
     $rockpop = $connekt->query($insertArtistsPop);
+
     if(!$rockpop){
         echo 'Cursed-Crap. Could not insert artists popularity.';
     }
@@ -54,11 +67,9 @@ function addArtist ($getThisArtist) {
     else {
         echo '<tr><td>' . $artistArt . '</td><td>' . $artistName . '</td><td>' . $artistPop . '</td></tr>';
     }
-	};	
-}
+};
 
-addArtist ($getThisArtist);
-
+addArtist ($thisArtist);
 die();
 
 ?>
