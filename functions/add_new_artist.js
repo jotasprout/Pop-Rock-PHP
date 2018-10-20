96f32ff0284b38cb9465962650a82b18947fe922
JotaSprout <jotasprout@gmail.com>
2018-10-20 15:24:45 -0400

    New Samples and Things to Try

--- functions/add_new_artist.js
+++ functions/add_new_artist.js
@@ -32,3 +32,85 @@ function submitArtist (event) {
   .catch((err) => console.log (err));
 
 }; // end of sendartistToServer
+
+// SEOMTHING I FOUND
+
+// Use jQuerys AJAX function to send data to a PHP Service:
+
+$.ajax({
+        type: 'POST',
+        url: path + '/php/yourService.php',
+        data: 'var=' + var,
+        success: function (response) {
+           // Do smth with the response
+        }
+    });
+
+// In your PHP Service read out data of the database:
+
+// connect to your database first
+
+$username = $_POST["var"];  
+$sql="SELECT * FROM users WHERE anything = '$var'";
+$result = mysql_query($sql);
+
+if($result === FALSE) {
+    die(mysql_error());
+}
+
+while($row = mysql_fetch_array($result)){ 
+    return $row["variable"];
+}
+
+// ANOTHER THING I FOUND
+
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('authors');
+const url = 'https://randomuser.me/api/?results=10';
+fetch(url)
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li'),
+        img = createNode('img'),
+        span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(JSON.stringify(error));
+}); 
+
+// AND ALSO THIS
+
+var poutput = $('.listHolder');
+
+$.ajax({
+url: 'https://domain.com/page.php',
+    dataType: 'jsonp',
+    jsonp: 'jsoncallback',
+    timeout: 5000,
+    success: function(data, status){	
+        $.each(data, function(pi,item){ 
+        str = item.name;	
+            var products = '<div id="'+item.id+'" class="items">'+
+                            '<p class="names">'+item.name+'</p>'+
+                            '</div>';
+            
+            poutput.append(products);
+                
+        })
+    }
+});
