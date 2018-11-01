466a60c7df4b6ebccc9ea43fbdd95aa1e0a8bfd4
JotaSprout <jotasprout@gmail.com>
2018-10-31 21:47:11 -0400

    troubleshooting genres

--- functions/genres.php
+++ functions/genres.php
@@ -1,13 +1,10 @@
 <?php
-
 session_start();
-
 require '../secrets/spotifySecrets.php';
 require '../vendor/autoload.php';
 require_once '../rockdb.php';
 require_once '../functions/artists.php';
 require_once '../data_text/artists_arrays_objects.php';
-
 $session = new SpotifyWebAPI\Session($myClientID, $myClientSecret);
 $session->requestCredentialsToken();
 $accessToken = $session->getAccessToken();
@@ -19,27 +16,42 @@ $GLOBALS['api'] = new SpotifyWebAPI\SpotifyWebAPI();
 $GLOBALS['api']->setAccessToken($accessToken);
 $baseURL = "https://api.spotify.com/v1/artists/";
 
+
 $thisArtist = '3D4qYDvoPn5cQxtBm4oseo';
 
 function addGenres ($thisArtist) {
 
-    echo $thisArtist . '<br>';
+    // echo $thisArtist . '<br>';
 
     $artist = $GLOBALS['api']->getArtist($thisArtist);
+    // echo $artist;
+
+    $connekt = new mysqli($GLOBALS['host'], $GLOBALS['un'], $GLOBALS['magicword'], $GLOBALS['db']);
+
+    if(!$connekt){
+        echo 'Fiddlesticks! Could not connect to database.<br>';
+    }
 
     $artistID = $artist->id;
-    // echo $artistID;
+    echo $artistID . '<br>';
     $artistNameYucky = $artist->name;
     $artistName = mysqli_real_escape_string($connekt,$artistNameYucky);
     echo $artistName . '<br>';
     //$artistGenres = $artist->genres[0]->url;
-    // $artistGenresArray = array();
+    $artistGenresArray = array();
 
-    $thisArtistGenres = $artist->genres;
-    echo $thisArtistGenres . '<br>';
+    $jsonArtistGenres = $artist->genres;
+    $phpArtistGenres = json_decode($jsonArtistGenres, true);
+    // print_r($phpArtistGenres);
+    
 
-    foreach ($thisArtistGenres->genres as $genre) {
-				
+    for ($i=0; $i<(count($phpArtistGenres)); ++$i {
+        echo $jsonArtistGenres[$i]; 
+    }
+/*
+    foreach ($phpArtistGenres->genres as $genre) {
+        echo $genre;	
+	
         $insertArtistGenres = "INSERT INTO genres (artistID,genre) VALUES('$artistID','$genre')";
 
         $rockout = $connekt->query($insertArtistGenres);
@@ -49,16 +61,15 @@ function addGenres ($thisArtist) {
         } else {
             echo $artistName . ' has the genre of ' . $genre . '<br>';
         }
+
     }
+    // echo $artistGenresArray;
+ */   
+
 }
 
-$connekt = new mysqli($GLOBALS['host'], $GLOBALS['un'], $GLOBALS['magicword'], $GLOBALS['db']);
 
-if(!$connekt){
-    echo 'Fiddlesticks! Could not connect to database.<br>';
-} else {
-    addGenres ($thisArtist);
-}
+addGenres ($thisArtist);
 
 die();
 
