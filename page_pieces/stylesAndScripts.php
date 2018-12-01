617ec7368cf45a1cdb3533d0d1811b6b555d5834
JotaSprout <jotasprout@gmail.com>
2018-12-01 13:47:15 -0500

    Add Favicons Icons and relevant code

--- page_pieces/stylesAndScripts.php
+++ page_pieces/stylesAndScripts.php
@@ -3,13 +3,14 @@
 $stylesAndSuch = "
     <meta charset='UTF-8'> 
     <link rel='stylesheet' type='text/css' href='https://www.roxorsoxor.com/bootstrap/css/bootstrap.min.css'>
-    <!--
-    <link rel='stylesheet' type='text/css' href='https://www.roxorsoxor.com/bootstrap/css/justified-nav.css'>
-    -->
     <script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
     <script src='https://code.jquery.com/ui/1.12.1/jquery-ui.js'></script>
     <link rel='stylesheet' href='https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css'>
     <link rel='stylesheet' href='https://www.roxorsoxor.com/poprock/css/pointy_head.css'>
+
+    <link rel='icon' type='image/png' href='/imgs/app-icon-128' sizes='128x128'>
+    <link rel='shortcut icon' href='favicon.ico'>
+
     <script src='https://d3js.org/d3.v4.min.js'></script>
     <script src='https://d3js.org/d3-time-format.v2.min.js'></script>
     
