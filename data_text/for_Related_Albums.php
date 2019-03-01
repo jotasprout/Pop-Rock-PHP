89a0f88ba26fef85700284a60d94c9a1514d57db
jotasprout <jotasprout@gmail.com>
2019-03-01 16:11:09 -0500

    Combining all code to combine all data

---
+++ data_text/for_Related_Albums.php
@@ -0,0 +1,39 @@
+<?php
+
+$related = array (
+    '1Fmb52lZ6Jv7FMWXXTPO3K', # Joan Jett
+    '5eTq3PxbOh5vgeRXKNqPyV', # Runaways
+    '5NhjPre67qjeeQP4KHDHpe', # Evil Stig
+    '1inWec2E2UgfzMAhwjgTXe', # Generation X
+    '7lzordPuZEXxwt9aoVZYmG', # Billy Idol
+    '3k4YA0uPsWc2PuOQlJNpdH', # Hollywood Vampires  
+    '3EhbVgyfGd7HkpsagwL9GS', # Alice Cooper 
+    '0p9uD4WGPHqMicwXm3Kavk', # Chagall Guevara
+    '0t1uzfQspxLvAifZLdmFe2', # Steve Taylor
+    '5MQsxr7sbsewUTIEEYxauR', # Steve Taylor and the Perfect Foil
+    '0PGxNwykt4KgnvSnNHVUSZ', # Steve Taylor and the Danielson Foil
+    '21ysNsPzHdqYN2fQ75ZswG', # Ted Nugent
+    '5cVLuEqb7aOHuzwssXHzWI', # Amboy Dukes 
+    '6ZLTlhejhndI4Rh53vYhrY', # Ozzy
+    '4UjiBRkTw9VmvDZiJZKPJ7', # Heaven & Hell
+    '4CYeVo5iZbtYGBN4Isc3n6', # Dio
+    '5M52tdBnJaKSvOpJGz8mfZ', # Black Sabbath
+    '6SLAMfhOi7UJI0fMztaK0m', # Rainbow
+    '2UZMlIwnkgAEDBsw1Rejkn', # Tom Petty
+    '4tX2TplrkIP4v05BNC903e', # Tom Petty & the Heartbreakers
+    '2hO4YtXUFJiUYS2uYFvHNK', # Traveling Wilburys
+    '33EUXrFKGjpUSGacqEHhU4', # Iggy Pop
+    '1l8grPt6eiOS4YlzjIs0LF', # Iggy Pop & James Williamson
+    '4BFMTELQyWJU1SwqcXMBm3', # Stooges
+    '2y8Jo9CKhJvtfeKOsYzRdT', # Rod Stewart 
+    # waiting for FACES
+    # Knott
+    '20ge6LnG4KYzVj0Ecj7yDv', 
+    '0yOXtSWTf2JZEbeBXD4wZa', 
+    '4VCZkmckTZMDFU0WsaepBe', 
+    '1p8SnxXwTByLcpQrCZ9c2J', 
+    '6sYiiBcFhVF76DR2BlRfcd', 
+    '0HdS9FxnRaiyuBjHBDo4vW'
+);
+
+?>
