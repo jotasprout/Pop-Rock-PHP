134385b9d90b6d230d33da5d78c902d0597ff94c
jotasprout <jotasprout@gmail.com>
2019-03-03 07:40:45 -0500

    troubleshooting Stoney and Meat Loaf. Otherwise, inserter works great.

--- functions/filenamesIntoMyDB.php
+++ functions/filenamesIntoMyDB.php
@@ -1,32 +1,44 @@
 <?php
-/*
-$filename = $_POST['filename'];
-echo $filename;
-*/
+
 require_once '../rockdb.php';
 require_once '../page_pieces/navbar_rock.php';
 require_once '../page_pieces/stylesAndScripts.php';
+
 /*
 $artistsMatchSpotifyMBID_Lookup = 'artistsMatchSpotifyMBID';
-
 $artistListenersPlaycount = 'artistListenersPlaycount';
 $albumListenersPlaycount = 'albumListenersPlaycount';
 $trackListenersPlaycount = 'trackListenersPlaycount';
-
 $relatedAlbums = 'relatedAlbums';
 $relatedArtists = 'relatedArtists';
+$jsonFile="../data_text/Anvil_Group_03-01-19.json";
+
+    '../data_text/AliceCooper_Combined_03-01-19.json', 
+    '../data_text/AliceCooper_Combined_02-15-19.json', 
+    '../data_text/AliceCooper_Combined_02-28-19.json', 
+    '../data_text/JoanJett_Combined_03-01-19.json', 
+    '../data_text/JoanJett_Combined_02-16-19.json', 
+    '../data_text/JoanJett_Combined_02-28-19.json', 
+    '../data_text/JoanJett_Combined_02-27-19.json',   
+    '../data_text/Anvil_02-15-19.json',      
+    '../data_text/Anvil_02-17-19.json',
+    '../data_text/Anvil_02-27-19.json',
+    '../data_text/Anvil_02-28-19.json',
+    '../data_text/Anvil_Group_03-01-19.json',
+    '../data_text/Elf_02-14-19.json', 
+    '../data_text/EvilStig_02-17-19.json', 
+    '../data_text/Heaven&Hell_02-14-19.json',
+
 */
-// $jsonFile="../data_text/Anvil_Group_03-01-19.json";
 
-/*
-$filenames = array ('../data_text/BlackSabbath_02-28-19.json', '../data_text/BlackSabbath_02-27-19.json', '../data_text/BlackSabbath_02-26-19.json', '../data_text/BlackSabbath_02-25-19.json', '../data_text/BlackSabbath_02-24-19.json', '../data_text/BlackSabbath_02-22-19.json', '../data_text/BlackSabbath_02-21-19.json', '../data_text/BlackSabbath_02-17-19.json');
-
-$filenamesKindaBroken = array ('../data_text/BlackSabbath_Group_03-01-19.json', '../data_text/BlackSabbath_Group_02-28-19.json', '../data_text/BlackSabbath_Group_02-27-19.json', '../data_text/BlackSabbath_Group_02-26-19.json', '../data_text/BlackSabbath_Group_02-25-19.json', '../data_text/BlackSabbath_Group_02-24-19.json', '../data_text/BlackSabbath_Group_02-22-19.json', '../data_text/BlackSabbath_Group_02-21-19.json', '../data_text/BlackSabbath_Group_02-17-19.json', '../data_text/Dio_Group_03-01-19.json', '../data_text/Dio_02-28-19.json', '../data_text/Dio_02-27-19.json', '../data_text/Dio_02-26-19.json', '../data_text/Dio_02-25-19.json', '../data_text/Dio_02-24-19.json', '../data_text/Dio_02-22-19.json', '../data_text/Dio_02-21-19.json', '../data_text/Dio_02-17-19.json', '../data_text/Elf_Group_03-01-19.json', '../data_text/Elf_02-28-19.json', '../data_text/Elf_02-27-19.json', '../data_text/Elf_02-26-19.json', '../data_text/Elf_02-25-19.json', '../data_text/Elf_02-24-19.json', '../data_text/Elf_02-22-19.json', '../data_text/Elf_02-21-19.json', '../data_text/Elf_02-17-19.json', '../data_text/EvilStig_Group_03-01-19.json', '../data_text/EvilStig_02-28-19.json', '../data_text/EvilStig_02-27-19.json', '../data_text/EvilStig_02-26-19.json', '../data_text/EvilStig_02-25-19.json', '../data_text/EvilStig_02-24-19.json', '../data_text/EvilStig_02-22-19.json', '../data_text/EvilStig_02-21-19.json', '../data_text/EvilStig_02-17-19.json', '../data_text/EvilStig_021619.json', '../data_text/Heaven&Hell_Group_03-01-19.json', '../data_text/Heaven&Hell_02-28-19.json', '../data_text/Heaven&Hell_02-27-19.json', '../data_text/Heaven&Hell_02-26-19.json', '../data_text/Heaven&Hell_02-25-19.json', '../data_text/Heaven&Hell_02-24-19.json', '../data_text/Heaven&Hell_02-22-19.json', '../data_text/Heaven&Hell_02-21-19.json', '../data_text/Heaven&Hell_02-17-19.json', '../data_text/MeatLoaf_Person_03-01-19.json', '../data_text/MeatLoaf_02-28-19.json', '../data_text/MeatLoaf_02-27-19.json', '../data_text/MeatLoaf_02-26-19.json', '../data_text/MeatLoaf_02-25-19.json', '../data_text/MeatLoaf_02-24-19.json', '../data_text/MeatLoaf_02-22-19.json', '../data_text/MeatLoaf_02-21-19.json', '../data_text/MeatLoaf_02-17-19.json', '../data_text/MötleyCrüe_Group_03-01-19.json', '../data_text/MötleyCrüe_02-28-19.json', '../data_text/MötleyCrüe_02-27-19.json', '../data_text/MötleyCrüe_02-25-19.json', '../data_text/OzzyOsbourne_Person_03-01-19.json', '../data_text/OzzyOsbourne_02-28-19.json', '../data_text/OzzyOsbourne_02-27-19.json', '../data_text/OzzyOsbourne_02-26-19.json', '../data_text/OzzyOsbourne_02-25-19.json', '../data_text/OzzyOsbourne_02-24-19.json', '../data_text/OzzyOsbourne_02-22-19.json', '../data_text/OzzyOsbourne_02-21-19.json', '../data_text/OzzyOsbourne_02-17-19.json', '../data_text/Queen_Group_03-01-19.json', '../data_text/Queen_02-28-19.json', '../data_text/Queen_02-27-19.json', '../data_text/Queen_02-25-19.json', '../data_text/QuietRiot_Group_03-01-19.json', '../data_text/QuietRiot_02-28-19.json', '../data_text/Rainbow_Group_03-01-19.json', '../data_text/Rainbow_02-28-19.json', '../data_text/Rainbow_02-27-19.json', '../data_text/Rainbow_02-26-19.json', '../data_text/Rainbow_02-25-19.json', '../data_text/Rainbow_02-24-19.json', '../data_text/Rainbow_02-22-19.json', '../data_text/Rainbow_02-21-19.json', '../data_text/Rainbow_02-17-19.json', '../data_text/TedNugent_Person_03-01-19.json', '../data_text/TedNugent_02-28-19.json', '../data_text/TedNugent_02-27-19.json', '../data_text/TedNugent_02-26-19.json', '../data_text/TedNugent_02-25-19.json', '../data_text/TedNugent_02-24-19.json', '../data_text/TedNugent_02-22-19.json', '../data_text/TedNugent_02-21-19.json', '../data_text/TedNugent_02-17-19.json', '../data_text/TheAmboyDukes_Group_03-01-19.json', '../data_text/TheAmboyDukes_02-28-19.json', '../data_text/TheAmboyDukes_02-27-19.json', '../data_text/TheAmboyDukes_02-26-19.json', '../data_text/TheAmboyDukes_02-25-19.json', '../data_text/TheAmboyDukes_02-24-19.json', '../data_text/TheAmboyDukes_02-22-19.json', '../data_text/TheAmboyDukes_02-21-19.json', '../data_text/TheAmboyDukes_02-17-19.json', '../data_text/Saxon_Group_03-01-19.json', '../data_text/Saxon_02-28-19.json', '../data_text/Saxon_02-27-19.json', '../data_text/Saxon_02-26-19.json', '../data_text/Saxon_02-25-19.json', '../data_text/Saxon_02-24-19.json', '../data_text/Saxon_02-22-19.json', '../data_text/Saxon_02-21-19.json', '../data_text/Saxon_02-17-19.json', '../data_text/TheRunaways_Group_03-01-19.json', '../data_text/TheRunaways_02-28-19.json', '../data_text/TheRunaways_02-27-19.json', '../data_text/TheElectricElves_Group_03-01-19.json', '../data_text/TheElectricElves_02-28-19.json', '../data_text/TheElectricElves_02-27-19.json', '../data_text/TheElectricElves_02-26-19.json', '../data_text/TheElectricElves_02-25-19.json', '../data_text/TheElectricElves_02-24-19.json', '../data_text/TheElectricElves_02-22-19.json', '../data_text/TheElectricElves_02-21-19.json', '../data_text/TheElectricElves_02-17-19.json', '../data_text/Stoney&MeatLoaf_Group_03-01-19.json', '../data_text/Stoney&MeatLoaf_02-28-19.json', '../data_text/Stoney&MeatLoaf_02-27-19.json');
-*/
-
+$filenames = array (
+    '../data_text/Stoney&MeatLoaf_02-17-19.json',
+    '../data_text/Stoney&MeatLoaf_02-27-19.json',
+    '../data_text/Stoney&MeatLoaf_02-28-19.json',
+    '../data_text/Stoney&MeatLoaf_Group_03-01-19.json'
+);
 
 $x = ceil((count($filenames)));
-
 
 for ($i=0; $i<$x; ++$i) {
     $jsonFile = $filenames[$i];
@@ -35,9 +47,6 @@ for ($i=0; $i<$x; ++$i) {
     
     $artistMBID = $artistData['mbid'];
     $artistName = $artistData['name'];
-    // Below doesn't work -- find out why
-    // $artistNameYucky = $artistData['name'];
-    // $artistName = mysqli_real_escape_string($artistNameYucky);
     
     $dataDate = $artistData['date'];
     
@@ -64,6 +73,5 @@ for ($i=0; $i<$x; ++$i) {
     } 
     
 };
-
 
 ?>
