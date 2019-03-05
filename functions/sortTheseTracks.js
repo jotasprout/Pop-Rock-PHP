47fc0bec2789c5054ee75b4d374d284be5d037e5
JotaSprout <jotasprout@gmail.com>
2019-03-04 20:11:13 -0500

    troubleshooting broken sorting

--- functions/sortTheseTracks.js
+++ functions/sortTheseTracks.js
@@ -1,4 +1,7 @@
+console.log ("I am in the javascript sorting file")
+
 function sortColumn (columnName, columnOrder) {
+	console.log("I am inside the function");
 	$.ajax ({
 		url: "functions/sortTheseTracks.php",
 		data: "sortBy=" + columnName + "&order=" + columnOrder,
