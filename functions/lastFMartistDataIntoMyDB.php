2d36b61913cde3796cfe8fd23a5ca27826af9e83
jotasprout <jotasprout@gmail.com>
2019-03-08 11:33:37 -0500

    working on complete studio script and album data scripts

--- functions/lastFMartistDataIntoMyDB.php
+++ functions/lastFMartistDataIntoMyDB.php
@@ -12,29 +12,29 @@ $trackListenersPlaycount = 'trackListenersPlaycount';
 $relatedAlbums = 'relatedAlbums';
 $relatedArtists = 'relatedArtists';
 */
-$filenamesX = array (
-    '../data_text/jsonLastFM/AliceCooper_Combined_03-06-19.json',
-    '../data_text/jsonLastFM/Anvil_Group_03-06-19.json',
-    '../data_text/jsonLastFM/BlackSabbath_Group_03-06-19.json',
-    '../data_text/jsonLastFM/Dio_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/Elf_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/EvilStig_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/Heaven&Hell_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/JoanJett_Combined_03-06-19.json', 
-    '../data_text/jsonLastFM/MeatLoaf_Person_03-06-19.json', 
-    '../data_text/jsonLastFM/MötleyCrüe_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/OzzyOsbourne_Person_03-06-19.json', 
-    '../data_text/jsonLastFM/Queen_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/QuietRiot_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/Rainbow_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/RonnieDioandtheProphets_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/RonnieDioandtheRedCaps_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/Saxon_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/Stoney&Meatloaf_Group_03-06-19.json',
-    '../data_text/jsonLastFM/TedNugent_Person_03-06-19.json', 
-    '../data_text/jsonLastFM/TheAmboyDukes_Group_03-06-19.json',
-    '../data_text/jsonLastFM/TheElectricElves_Group_03-06-19.json', 
-    '../data_text/jsonLastFM/TheRunaways_Group_03-06-19.json'
+$filenames = array (
+    '../data_text/jsonLastFM/AliceCooper_Combined_03-08-19.json',
+    '../data_text/jsonLastFM/Anvil_Group_03-08-19.json',
+    '../data_text/jsonLastFM/BlackSabbath_Group_03-08-19.json',
+    '../data_text/jsonLastFM/Dio_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/Elf_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/EvilStig_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/Heaven&Hell_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/JoanJett_Combined_03-08-19.json', 
+    '../data_text/jsonLastFM/MeatLoaf_Person_03-08-19.json', 
+    '../data_text/jsonLastFM/MötleyCrüe_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/OzzyOsbourne_Person_03-08-19.json', 
+    '../data_text/jsonLastFM/Queen_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/QuietRiot_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/Rainbow_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/RonnieDioandtheProphets_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/RonnieDioandtheRedCaps_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/Saxon_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/Stoney&Meatloaf_Group_03-08-19.json',
+    '../data_text/jsonLastFM/TedNugent_Person_03-08-19.json', 
+    '../data_text/jsonLastFM/TheAmboyDukes_Group_03-08-19.json',
+    '../data_text/jsonLastFM/TheElectricElves_Group_03-08-19.json', 
+    '../data_text/jsonLastFM/TheRunaways_Group_03-08-19.json'
 );
 
 
