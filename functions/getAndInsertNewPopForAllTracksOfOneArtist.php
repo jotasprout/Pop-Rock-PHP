5974ca744feff3b76141d95f49298dca8fde6b86
jotasprout <jotasprout@gmail.com>
2019-03-11 17:49:03 -0400

    Get all artist tracks pop works

---
+++ functions/getAndInsertNewPopForAllTracksOfOneArtist.php
@@ -0,0 +1,57 @@
+<?php
+
+session_start();
+require '../secrets/auth.php';
+require_once '../rockdb.php';
+require_once '../rockdb.php';
+require_once 'tracks.php';
+
+$connekt = new mysqli( $GLOBALS[ 'host' ], $GLOBALS[ 'un' ], $GLOBALS[ 'magicword' ], $GLOBALS[ 'db' ] );
+
+if ( !$connekt ) {
+	echo 'Darn. Did not connect.';
+};
+
+$artistID = '3EhbVgyfGd7HkpsagwL9GS';
+
+$allTracks = array ();
+
+$gatherAllTrackIDsForThisArtist = "SELECT t.trackID, t.trackName, a.albumName, a.artistID
+						FROM tracks t
+						INNER JOIN albums a 
+						ON a.albumID = t.albumID
+						WHERE a.artistID = '$artistID'";
+
+$getit = $connekt->query( $gatherAllTrackIDsForThisArtist );
+
+if ( !$getit ) {
+	echo 'Cursed-Crap. Did not run the query.';
+};
+
+if(!empty($getit)) {
+
+	while ( $row = mysqli_fetch_array( $getit ) ) {
+		$trackID = $row[ "trackID" ];
+		$allTracks [] = $trackID;		
+	}; // end of while
+	
+	$session = new SpotifyWebAPI\Session($myClientID, $myClientSecret);
+
+	$session->requestCredentialsToken();
+	$accessToken = $session->getAccessToken();
+
+	// I don't think the cron needs this next line 
+	$_SESSION['accessToken'] = $accessToken;
+	// and I don't think the cron needs this next line either
+	$accessToken = $_SESSION['accessToken'];
+
+	$GLOBALS['api'] = new SpotifyWebAPI\SpotifyWebAPI();
+	$GLOBALS['api']->setAccessToken($accessToken);
+	
+	
+	divideCombineTracksAndInsertPop ($allTracks);
+} // end of if
+			
+?>
+
+
--- functions/tracks.php
+++ functions/tracks.php
@@ -74,4 +74,62 @@ function divideCombineInsertTracksAndPop ($AlbumsTracks) {
 }
 
 
+
+
+
+function divideCombineTracksAndInsertPop ($allArtistTracks) {
+
+	// Divide all artist's tracks into chunks of 50
+	$tracksChunk = array ();
+	$x = ceil((count($allArtistTracks))/50);
+
+	$firstTrack = 0;
+
+	for ($i=0; $i<$x; ++$i) {
+		$lastTrack = 49;
+		$tracksChunk = array_slice($allArtistTracks, $firstTrack, $lastTrack);
+		// put chunks of 50 into an array
+		$albumsTracksArrays [] = $tracksChunk;
+		$firstTrack += 50;
+	};
+
+	for ($i=0; $i<(count($albumsTracksArrays)); ++$i) {
+				
+		$tracksThisTime = count($albumsTracksArrays[$i]);
+		// echo $tracksThisTime . '<br>';
+
+		$trackIds = implode(',', $albumsTracksArrays[$i]);
+
+		// For each array of tracks (50 at a time), "get several tracks"
+		$bunchoftracks = $GLOBALS['api']->getTracks($trackIds);
+			
+		foreach ($bunchoftracks->tracks as $track) {
+			
+			$connekt = new mysqli($GLOBALS['host'], $GLOBALS['un'], $GLOBALS['magicword'], $GLOBALS['db']);
+			
+			if (!$connekt) {
+				echo 'Darn. Did not connect.';
+			};			
+
+			$trackID = $track->id;
+			$trackAlbumName = $track->album->name;
+			$trackName = $track->name;
+			$trackPop = $track->popularity;
+	
+			$insertTrackPop = "INSERT INTO popTracks (trackID,pop) VALUES('$trackID','$trackPop')";
+	
+			$rockpop = $connekt->query($insertTrackPop);
+			
+			if(!$rockpop){
+				echo 'Confounded-Crap. Could not insert POPULARITY for "' . $trackName . '" from <i>' . $trackAlbumName . '</i>.<br>';
+			}
+	
+			else {
+				echo $trackName . " from <i>" . $trackAlbumName . "</i>" . " has pop " . $trackPop . "<br>";
+			}
+		}
+	};
+}
+
+
 ?>
