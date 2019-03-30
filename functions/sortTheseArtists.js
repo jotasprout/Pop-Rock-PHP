bd5d9f117859b5fab667a3fb72be07d48d7e010e
jotasprout <jotasprout@gmail.com>
2019-03-30 09:59:34 -0400

    debugging

--- functions/sortTheseArtists.js
+++ functions/sortTheseArtists.js
@@ -1,5 +1,4 @@
 function sortColumn (columnName, currentOrder) {
-	console.log ("Column name is " + columnName + " and the current order is " + currentOrder);
 	$.ajax ({
 		url: "functions/sortTheseArtists.php",
 		data: "columnName=" + columnName + "&currentOrder=" + currentOrder,
