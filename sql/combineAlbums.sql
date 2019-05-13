8045310969f7e6ff443f1071cc5e29303803e10c
JotaSprout <jotasprout@gmail.com>
2019-05-13 17:44:51 -0400

    Comments in sql note doc

--- sql/combineAlbums.sql
+++ sql/combineAlbums.sql
@@ -2,8 +2,10 @@ SELECT albums.albumID, albums.albumName, albumsMB.albumName, albumsMB.albumMBID
 FROM albums
 LEFT JOIN albumsMB
 ON albumsMB.artistSpotID = albums.artistID AND albums.albumName = albumsMB.albumName
-# Above gets 5,742 rows (because it includes albums with no match?)
-# Changing it to INNER JOIN gets 231 rows
+/* 
+Above gets 5,742 rows (because it includes albums with no match?)
+Changing it to INNER JOIN gets 231 rows
+*/
 
 UPDATE albumsMB
 INNER JOIN albums ON albumsMB.artistSpotID = albums.artistID
@@ -14,20 +16,20 @@ SELECT m.albumName, m.artistSpotID, a.artistName, a.artistID
 FROM `albumsMB` m 
 JOIN artists a ON a.artistMBID = m.artistMBID 
 ORDER BY `artistSpotID` ASC
-# The above returns 486 rows
-# There are 524 albums in albumsMB
+/* 
+The above returns 486 rows
+There are 524 albums in albumsMB 
+*/
 
 SELECT albums.albumID, albums.albumName, albumsMB.albumName, albumsMB.albumMBID FROM albums LEFT JOIN albumsMB ON albums.albumName = albumsMB.albumName
-# Above returns 5,842 rows
+/* Above returns 5,842 rows */
 
 UPDATE albums
 INNER JOIN albumsMB ON albums.artistID = albumsMB.artistSpotID AND albums.albumName = albumsMB.albumName
 SET albums.albumMBID = albumsMB.albumMBID
 WHERE albums.artistID = albumsMB.artistSpotID AND albums.albumName = albumsMB.albumName
-# Above affected 231 rows
+/* Above affected 231 rows */
 
-
-#########################################
 
 
 UPDATE tracks 
@@ -36,4 +38,4 @@ FROM tracksMB
 JOIN albums ON tracksMB.albumMBID = albums.albumMBID) tmb ON tmb.trackName = tracks.trackName AND tmb.albumID = tracks.albumID
 SET tracks.trackMBID = tmb.trackMBID
 WHERE tmb.trackName = tracks.trackName AND tmb.albumID = tracks.albumID
-# Above 1649 rows affected. I sure do hope they are correct.
+/* Above 1649 rows affected. I sure do hope they are correct. */
